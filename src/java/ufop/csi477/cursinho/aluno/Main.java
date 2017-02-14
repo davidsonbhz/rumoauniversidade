5180c9828d2ede76acdaef5625d3fa4cbb828618
davidson <davidson@nb-davidson>
2017-02-14 10:51:51 -0200

    Commit inicial do projeto

---
+++ src/java/ufop/csi477/cursinho/aluno/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufop.csi477.cursinho.aluno;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ *
+ * @author davidson
+ */
+public class Main extends UI {
+
+    @Override
+    protected void init(VaadinRequest request) {
+        
+        
+    }
+    
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = Main.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+    
+}
